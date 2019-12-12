1e0c31a8c8091d5124851059f991fb577114af89
lihaixin <lihaixin@15099.net>
2019-12-12 12:29:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 echo "FRP_PORT is $FRP_PORT /tcp
-echo "KCP_FRP_PORT is $FRP_PORT /udp
+# echo "KCP_FRP_PORT is $FRP_PORT /udp
 echo "V_HTTP_PORT is $V_HTTP_PORT /tcp
 echo "V_HTTPS_PORT is $V_HTTPS_PORT /tcp
 /var/frp/frps -c /var/frp/conf/frps.ini &
