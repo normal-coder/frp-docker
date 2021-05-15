4d7f9aec81814ac14e9b90ebe4b4853b794138b7
lihaixin <lihaixin@15099.net>
2021-05-15 20:42:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+
+install_cert() {
+mkdir -p /etc/cert/$DOMAIN
+curl https://get.acme.sh | sh
+~/.acme.sh/acme.sh  --issue --dns dns_cf -d $DOMAIN -d *.$DOMAIN --key-file /etc/cert/$DOMAIN/private.key --fullchain-file /etc/cert/$DOMAIN/fullchain.crt
+~/.acme.sh/acme.sh --upgrade --auto-upgrade
+}
+
+# 查看证书，没有就自动创建
+if [ ! -f "/etc/cert/$DOMAIN/fullchain.crt" ]; then
+  install_cert
+fi
+
 /var/frp/frps -c /var/frp/conf/frps.ini &
 /var/frp/frpc -c /var/frp/conf/frpc.ini
